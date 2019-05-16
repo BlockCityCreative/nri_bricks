f1834fdec0fcec3a26ef1fabd89de89bcb46d5eb
nri <47117716+nri-1@users.noreply.github.com>
2019-05-16 14:35:02 -0500

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,69 @@
+minetest.register_node("nri_bricks:brown_brick", {
+	description = "Brown brick for building",
+	tiles = {
+		"brown_brick.png", -- top
+		"brown_brick.png", -- bottom
+		"brown_brick.png", -- right
+		"brown_brick.png", -- left
+		"brown_brick.png", -- back
+		"brown_brick.png"  -- front
+	},
+	groups = {oddly_breakable_by_hand = 2}
+})
+
+minetest.register_node("nri_bricks:black_brick", {
+	description = "Black brick for building",
+	tiles = {
+		"black_brick.png", -- top
+		"black_brick.png", -- bottom
+		"black_brick.png", -- right
+		"black_brick.png", -- left
+		"black_brick.png", -- back
+		"black_brick.png"  -- front
+	},
+	groups = {oddly_breakable_by_hand = 2}
+})
+
+minetest.register_node("nri_bricks:green_brick", {
+	description = "Green brick for building",
+	tiles = {
+		"green_brick.png", -- top
+		"green_brick.png", -- bottom
+		"green_brick.png", -- right
+		"green_brick.png", -- left
+		"green_brick.png", -- back
+		"green_brick.png"  -- front
+	},
+	groups = {oddly_breakable_by_hand = 2}
+})
+
+minetest.register_node("nri_bricks:white_brick", {
+	description = "White brick for building",
+	tiles = {
+		"white_brick.png", -- top
+		"white_brick.png", -- bottom
+		"white_brick.png", -- right
+		"white_brick.png", -- left
+		"white_brick.png", -- back
+		"white_brick.png"  -- front
+	},
+	groups = {oddly_breakable_by_hand = 2}
+})
+
+minetest.register_node("nri_bricks:082c2e_brick", {
+	description = "082c2e brick for building",
+	tiles = {
+		"082c2e_brick.png", -- top
+		"082c2e_brick.png", -- bottom
+		"082c2e_brick.png", -- right
+		"082c2e_brick.png", -- left
+		"082c2e_brick.png", -- back
+		"082c2e_brick.png"  -- front
+	},
+	groups = {oddly_breakable_by_hand = 2}
+})
+
+
+
+
+
